2df005b6a00fb3ed1775562f297dac6c8f89f6ae
Mikhail Myshakin <myshakinm@mail.ru>
2025-07-20 12:23:04 +0300

    edit name on about page

--- test/about.test.tsx
+++ test/about.test.tsx
@@ -1,11 +1,10 @@
-import React from "react";
 import { render } from "@testing-library/react";
 import { About } from "../src/client/pages/About";
 
 describe("About", () => {
-  it("Should render author name", () => {
-    const { container } = render(<About />);
+    it("Should render author name", () => {
+        const { container } = render(<About />);
 
-    expect(container.textContent).toBe("AboutAuthor: Nikita Usachev");
-  });
+        expect(container.textContent).toBe("AboutAuthor: [Nikita Usachev]");
+    });
 });
